4c4a39b2860e778ef4b18d120a62a99bbdcb091e
MacKentoch <erwan.datin@gmail.com>
2016-07-19 16:38:20 +0200

    fix getLastThreeMonthNames

--- src/app/services/utils/dateTools.js
+++ src/app/services/utils/dateTools.js
@@ -4,7 +4,7 @@ moment.locale('fr');
 
 // retourne un array des 3 derniers mois (en lettre FR) (mois en court compris)
 export const getLastThreeMonthNames = () => {
-  return [0, 1, 2].map(
-    delta => moment().add(delta, 'months').format('MMMM')
+  return [2, 1, 0].map(
+    delta => moment().subtract(delta, 'months').format('MMMM')
   );
 };
