8273783f62d4cc40d45979e3920888e32e260c6d
MacKentoch <erwan.datin@gmail.com>
2016-08-18 12:34:30 +0200

    attachments components

--- src/app/components/mailboxWriteMail/attachments/zip/Zip.js
+++ src/app/components/mailboxWriteMail/attachments/zip/Zip.js
@@ -1,31 +1,53 @@
-import React, { PropTypes } from 'react';
+import React, {
+  Component,
+  PropTypes
+}                     from 'react';
+import shallowCompare from 'react-addons-shallow-compare';
 
-const Zip = ({name, filePath, size, onTrashClick}) => {
-  return (
-    <li>
-      <span className="mailbox-attachment-icon">
-        <i className="fa fa-file-archive-o"></i>
-      </span>
-      <div className="mailbox-attachment-info">
-        <a
-          href={filePath}
-          className="mailbox-attachment-name">
-          <i className="fa fa-paperclip"></i>
-          &nbsp;
-          {name}
-        </a>
-        <span className="mailbox-attachment-size">
-          {size}
-          <button
-            className="btn btn-default btn-xs pull-right"
-            onClick={onTrashClick}>
-            <i className="fa fa-trash-o"></i>
-          </button>
+class Zip extends Component {
+  constructor(props) {
+    super(props);
+    this.handlesOnTrashClick = this.handlesOnTrashClick.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState);
+  }
+
+  render() {
+    const { name, filePath, size } = this.props;
+    return (
+      <li>
+        <span className="mailbox-attachment-icon">
+          <i className="fa fa-file-archive-o"></i>
         </span>
-      </div>
-    </li>
-  );
-};
+        <div className="mailbox-attachment-info">
+          <a
+            href={filePath}
+            className="mailbox-attachment-name">
+            <i className="fa fa-paperclip"></i>
+            &nbsp;
+            {name}
+          </a>
+          <span className="mailbox-attachment-size">
+            {size}
+            <button
+              className="btn btn-default btn-xs pull-right"
+              onClick={this.handlesOnTrashClick}>
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </span>
+        </div>
+      </li>
+    );
+  }
+
+  handlesOnTrashClick(evt) {
+    evt.preventDefault();
+    const { onTrashClick, name } = this.props;
+    onTrashClick(name);
+  }
+}
 
 Zip.propTypes = {
   name: PropTypes.string.isRequired,
