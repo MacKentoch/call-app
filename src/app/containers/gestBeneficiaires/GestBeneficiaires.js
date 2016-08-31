da82d121e536a6ddf4d63ce52a1e44fb30189a63
MacKentoch <erwan.datin@gmail.com>
2016-08-31 11:53:50 +0200

    GestBeneficiairesConnected update

--- src/app/containers/gestBeneficiaires/GestBeneficiaires.js
+++ src/app/containers/gestBeneficiaires/GestBeneficiaires.js
@@ -5,7 +5,32 @@ import { GestBeneficiaires }    from '../../views';
 
 const mapStateToProps = (state) => {
   return {
-    currentView:  state.views.currentView
+    currentView:  state.views.currentView,
+    // gestBenef data:
+    isFetching: state.gestBenef.isFetching,
+    lastGetTime: state.gestBenef.time,
+    // identité
+    civilite: state.gestBenef.civilite,
+    nom: state.gestBenef.nom,
+    nomJeuneFille: state.gestBenef.nomJeuneFille,
+    prenom: state.gestBenef.prenom,
+    dateNaissance: state.gestBenef.dateNaissance,
+    numss: state.gestBenef.numss,
+    dateDeces: state.gestBenef.dateDeces,
+    maritalStatus: state.gestBenef.maritalStatus,
+    // contact data
+    fixedPhone: state.gestBenef.fixedPhone,
+    mobilePhone: state.gestBenef.mobilePhone,
+    email: state.gestBenef.email,
+    // contact data > sub: adress
+    numAdress: state.gestBenef.numAdress,
+    voie: state.gestBenef.voie,
+    complementAdr: state.gestBenef.complementAdr,
+    codePostal: state.gestBenef.codePostal,
+    ville: state.gestBenef.ville,
+    pays: state.gestBenef.pays,
+    // dossiers:
+    dossiers: state.gestBenef.dossiers
   };
 };
 
@@ -14,7 +39,9 @@ const mapDispatchToProps = (dispatch) => {
     actions : bindActionCreators(
       {
         enterGestBeneficiaires: actions.enterGestBeneficiaires,
-        leaveGestBeneficiaires: actions.leaveGestBeneficiaires
+        leaveGestBeneficiaires: actions.leaveGestBeneficiaires,
+        // get gestBenef:
+        getGestBenefIfNeeded: actions.getGestBenefIfNeeded
       },
       dispatch)
   };
