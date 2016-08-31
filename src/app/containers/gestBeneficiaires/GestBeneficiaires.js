792a5d1630ee3384778882d796cf51b2a6c8d66a
MacKentoch <erwan.datin@gmail.com>
2016-08-31 17:19:56 +0200

    GestBeneficiaires container new props

--- src/app/containers/gestBeneficiaires/GestBeneficiaires.js
+++ src/app/containers/gestBeneficiaires/GestBeneficiaires.js
@@ -6,10 +6,18 @@ import { GestBeneficiaires }    from '../../views';
 const mapStateToProps = (state) => {
   return {
     currentView:  state.views.currentView,
+    // general info
+    lastActionTime: state.gestBenef.actionTime,
     // gestBenef data:
-    isFetching: state.gestBenef.isFetching,
-    lastGetTime: state.gestBenef.time,
+    // isFetching: state.gestBenef.isFetching,
+    // lastGetTime: state.gestBenef.time,
+    id: state.gestBenef.id,
     // identité
+    isFetchingIdentite: state.gestBenef.isFetchingIdentite,
+    lastFetchTimeIdentite: state.gestBenef.lastFetchTimeIdentite,
+    isEditingIdentite: state.gestBenef.isEditingIdentite,
+    isSavingIdentite: state.gestBenef.isSavingIdentite,
+    isCollapsedIdentite: state.gestBenef.isCollapsedIdentite,
     civilite: state.gestBenef.civilite,
     nom: state.gestBenef.nom,
     nomJeuneFille: state.gestBenef.nomJeuneFille,
@@ -19,6 +27,11 @@ const mapStateToProps = (state) => {
     dateDeces: state.gestBenef.dateDeces,
     maritalStatus: state.gestBenef.maritalStatus,
     // contact data
+    isFetchingContact: state.gestBenef.isFetchingContact,
+    lastFetchTimeContact: state.gestBenef.lastFetchTimeContact,
+    isEditingContact: state.gestBenef.isEditingContact,
+    isSavingContact: state.gestBenef.isSavingContact,
+    isCollapsedContact: state.gestBenef.isCollapsedContact,
     fixedPhone: state.gestBenef.fixedPhone,
     mobilePhone: state.gestBenef.mobilePhone,
     email: state.gestBenef.email,
@@ -30,6 +43,11 @@ const mapStateToProps = (state) => {
     ville: state.gestBenef.ville,
     pays: state.gestBenef.pays,
     // dossiers:
+    isFetchingDossiers: state.gestBenef.isFetchingDossiers,
+    lastFetchTimeDossiers: state.gestBenef.lastFetchTimeDossiers,
+    isEditingDossiers: state.gestBenef.isEditingDossiers,
+    isSavingDossiers: state.gestBenef.isSavingDossiers,
+    isCollapsedDossiers: state.gestBenef.isCollapsedDossiers,
     dossiers: state.gestBenef.dossiers
   };
 };
@@ -41,7 +59,9 @@ const mapDispatchToProps = (dispatch) => {
         enterGestBeneficiaires: actions.enterGestBeneficiaires,
         leaveGestBeneficiaires: actions.leaveGestBeneficiaires,
         // get gestBenef:
-        getGestBenefIfNeeded: actions.getGestBenefIfNeeded
+        getGestBenefIfNeeded: actions.getGestBenefIfNeeded,
+        // post
+        postGestBenefIdentiteIfNeeded: actions.postGestBenefIdentiteIfNeeded
       },
       dispatch)
   };
