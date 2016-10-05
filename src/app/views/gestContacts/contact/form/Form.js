d673d9412b645c80481dc597a59f56d8ee192011
MacKentoch <erwan.datin@gmail.com>
2016-10-05 14:30:01 +0200

    contact from

--- src/app/views/gestContacts/contact/form/Form.js
+++ src/app/views/gestContacts/contact/form/Form.js
@@ -1,35 +1,16 @@
 import React, { PropTypes } from 'react';
-import moment               from 'moment';
-// import { appConfig }        from '../../../../config';
-import {
-  FormLabel,
-  TextInput,
-  TelephoneInput
-}                           from '../../../../components';
-
-moment.locale('fr');
-// const formatDate = appConfig.formatDate.defaut;
+import { FormLabel }        from '../../../../components';
 
 const Form = ({
-  isEditingContact,
   fixedPhone,
-  onFixedPhoneChanged,
   mobilePhone,
-  onMobilePhoneChanged,
   email,
-  onEmailChanged,
   numAdress,
-  onNumAdressChanged,
   voie,
-  onVoieChanged,
   complementAdr,
-  onComplementAdrChanged,
   codePostal,
-  onCodePostalChanged,
   ville,
-  onVilleChanged,
-  pays,
-  onPaysChanged
+  pays
 }) => {
   return (
     <form role="form">
@@ -38,41 +19,20 @@ const Form = ({
       <div className="row">
         {/* fixedPhone */}
         <div className="col-md-6">
-          {
-            isEditingContact
-            ?
-              <TelephoneInput
-                id="inputFixedPhone"
-                label={'Téléphone fixe'}
-                value={fixedPhone}
-                onChange={onFixedPhoneChanged}
-              />
-            :
-              <FormLabel
-                id="inputFixedPhone"
-                label={'Téléphone fixe'}
-                value={fixedPhone || ' --- '}
-              />
-          }
+          <FormLabel
+            id="inputFixedPhone"
+            label={'Téléphone fixe'}
+            value={fixedPhone || ' --- '}
+          />
         </div>
+
         {/* mobilePhone */}
         <div className="col-md-6">
-          {
-            isEditingContact
-            ?
-              <TelephoneInput
-                id="inputMobilePhone"
-                label={'Téléphone mobile'}
-                value={mobilePhone}
-                onChange={onMobilePhoneChanged}
-              />
-            :
-              <FormLabel
-                id="inputMobilePhone"
-                label={'Téléphone mobile'}
-                value={mobilePhone}
-              />
-          }
+          <FormLabel
+            id="inputMobilePhone"
+            label={'Téléphone mobile'}
+            value={mobilePhone}
+          />
         </div>
       </div>
 
@@ -80,22 +40,11 @@ const Form = ({
       <div className="row">
         {/* email */}
         <div className="col-md-12">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputEmail"
-                label={'Adresse mail'}
-                value={email}
-                onChange={onEmailChanged}
-              />
-            :
-              <FormLabel
-                id="inputEmail"
-                label={'Adresse mail'}
-                value={email}
-              />
-          }
+          <FormLabel
+            id="inputEmail"
+            label={'Adresse mail'}
+            value={email}
+          />
         </div>
       </div>
 
@@ -103,60 +52,27 @@ const Form = ({
       <div className="row">
         {/* numAdress */}
         <div className="col-md-3">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputNumAdr"
-                label={'N°'}
-                value={numAdress}
-                onChange={onNumAdressChanged}
-              />
-            :
-              <FormLabel
-                id="inputNumAdr"
-                label={'N°'}
-                value={numAdress}
-              />
-          }
+          <FormLabel
+            id="inputNumAdr"
+            label={'N°'}
+            value={numAdress}
+          />
         </div>
         {/* voie */}
         <div className="col-md-5">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputVoie"
-                label={'Voie'}
-                value={voie}
-                onChange={onVoieChanged}
-              />
-            :
-              <FormLabel
-                id="inputVoie"
-                label={'Voie'}
-                value={voie}
-              />
-          }
+          <FormLabel
+            id="inputVoie"
+            label={'Voie'}
+            value={voie}
+          />
         </div>
         {/* complementAdr */}
         <div className="col-md-4">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputComplementAdr"
-                label={'Complément'}
-                value={complementAdr}
-                onChange={onComplementAdrChanged}
-              />
-            :
-              <FormLabel
-                id="inputComplementAdr"
-                label={'Complément'}
-                value={complementAdr}
-              />
-          }
+          <FormLabel
+            id="inputComplementAdr"
+            label={'Complément'}
+            value={complementAdr}
+          />
         </div>
       </div>
 
@@ -165,60 +81,27 @@ const Form = ({
       <div className="row">
         {/* codePostal */}
         <div className="col-md-3">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputCodePostal"
-                label={'Code postal'}
-                value={codePostal}
-                onChange={onCodePostalChanged}
-              />
-            :
-              <FormLabel
-                id="inputCodePostal"
-                label={'Code postal'}
-                value={codePostal}
-              />
-          }
+          <FormLabel
+            id="inputCodePostal"
+            label={'Code postal'}
+            value={codePostal}
+          />
         </div>
         {/* ville */}
         <div className="col-md-5">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputVille"
-                label={'Ville'}
-                value={ville}
-                onChange={onVilleChanged}
-              />
-            :
-              <FormLabel
-                id="inputVille"
-                label={'Ville'}
-                value={ville}
-              />
-          }
+          <FormLabel
+            id="inputVille"
+            label={'Ville'}
+            value={ville}
+          />
         </div>
         {/* pays */}
         <div className="col-md-4">
-          {
-            isEditingContact
-            ?
-              <TextInput
-                id="inputPays"
-                label={'Pays'}
-                value={pays}
-                onChange={onPaysChanged}
-              />
-            :
-              <FormLabel
-                id="inputPays"
-                label={'Pays'}
-                value={pays}
-              />
-          }
+          <FormLabel
+            id="inputPays"
+            label={'Pays'}
+            value={pays}
+          />
         </div>
       </div>
 
@@ -227,34 +110,23 @@ const Form = ({
 };
 
 Form.propTypes = {
-  isEditingContact: PropTypes.bool,
-
   fixedPhone: PropTypes.string.isRequired,
-  onFixedPhoneChanged: PropTypes.func.isRequired,
 
   mobilePhone: PropTypes.string.isRequired,
-  onMobilePhoneChanged: PropTypes.func.isRequired,
 
   email: PropTypes.string.isRequired,
-  onEmailChanged: PropTypes.func.isRequired,
 
   numAdress: PropTypes.string.isRequired,
-  onNumAdressChanged: PropTypes.func.isRequired,
 
   voie: PropTypes.string.isRequired,
-  onVoieChanged: PropTypes.func.isRequired,
 
   complementAdr: PropTypes.string.isRequired,
-  onComplementAdrChanged: PropTypes.func.isRequired,
 
   codePostal: PropTypes.string.isRequired,
-  onCodePostalChanged: PropTypes.func.isRequired,
 
   ville: PropTypes.string.isRequired,
-  onVilleChanged: PropTypes.func.isRequired,
 
-  pays: PropTypes.string.isRequired,
-  onPaysChanged: PropTypes.func.isRequired
+  pays: PropTypes.string.isRequired
 };
 
 export default Form;
