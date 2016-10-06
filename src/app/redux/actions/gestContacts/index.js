a850680cafcd19356f1479076c1fa182d5a0377e
MacKentoch <erwan.datin@gmail.com>
2016-10-06 08:30:30 +0200

    gestContacts actions creator index

--- src/app/redux/actions/gestContacts/index.js
+++ src/app/redux/actions/gestContacts/index.js
@@ -1,68 +1,19 @@
-// benef all:
-import { getGestBenefIfNeeded, resetGestBenef } from './gestBenef';
-// benef Idnetite:
+// contacts all:
+import { getGestContactsIfNeeded, resetGestBenef } from './gestBenef';
+// contacts Idnetite:
 import {
-  resetGestBenefIdentite,
-
-  getGestBenefIdentiteIfNeeded,
-  postGestBenefIdentiteIfNeeded,
-
-  setIsEditingIdentite,
-  unsetIsEditingIdentite,
-
   setIsCollapsedIdentite,
-  unsetIsCollapsedIdentite,
-
-  updateCiviliteIdentite,
-  updateNomIdentite,
-  updateNomDeJeuneFilleIdentite,
-  updatePrenomIdentite,
-  updateDateNaissanceIdentite,
-  updateNumssIdentite,
-  updateDateDecesIdentite,
-  updateMaritalStatusIdentite
+  unsetIsCollapsedIdentite
 }                               from './gestBenefIdentite/gestBenefIdentite';
-// benef contact:
+// contacts contact:
 import {
-  resetGestBenefContact,
-
-  getGestBenefContactIfNeeded,
-  postGestBenefContactIfNeeded,
-
-  setIsEditingContact,
-  unsetIsEditingContact,
-
   setIsCollapsedContact,
-  unsetIsCollapsedContact,
-
-  updateTelephoneFixeContact,
-  updateTelephoneMobileContact,
-  updateEmailContact,
-  updateNumAdressNumber,
-  updateVoieAdressContact,
-  updateComplementAdressContact,
-  updateCodePostalContact,
-  updateVilleContact,
-  updatePaysContact
+  unsetIsCollapsedContact
 }                               from './gestBenefContact/gestBenefContact';
-// benef dossiers:
+// contacts dossiers:
 import {
-  getGestBenefAllDossiersIfNeeded,
-
   setIsCollapsedDossiers,
-  unsetIsCollapsedDossiers,
-
-  setIsSavingNewDossier,
-  unsetIsSavingNewDossier,
-
-  setIsEditingDossier,
-  unsetIsEditingDossier,
-
-  addGestBenefNewDossierIfNeeded,
-
-  updateGestBenefDossierIfNeeded,
-
-  resetGestBenefDossier
+  unsetIsCollapsedDossiers
 }                               from './gestBenefDossiers/gestBenefDossiers';
 import {
   getGestBenefAllContactsEtActivitesIfNeeded,
@@ -72,8 +23,8 @@ import {
   unsetIsCollapsedContactsEtActivites
 }                               from './gestBenefContactsEtActivites/gestBenefContactsEtActivites';
 export {
-  // all benef fields (identite + contact + dossiers)
-  getGestBenefIfNeeded,
+  // all contacts (benef info) fields (identite + contact + dossiers)
+  getGestContactsIfNeeded,
   // benef identite only
   resetGestBenefIdentite,
   getGestBenefIdentiteIfNeeded,
