367136904fdb918ab5ca9e6521ab1f0f6b201a09
MacKentoch <erwan.datin@gmail.com>
2016-10-11 09:24:06 +0200

    gestContacts

--- src/app/redux/reducers/gestContacts/gestContacts.js
+++ src/app/redux/reducers/gestContacts/gestContacts.js
@@ -307,6 +307,108 @@ const gestContacts = (state = initialState, action) => {
       actionTime: action.time
     };
 
+  case REQUEST_GET_GEST_CONTACTS_FICHE_CONTACT:
+    return {
+      ...state,
+      isFetchingContact: action.isFetchingContact,
+      lastFetchTimeContact: action.time,
+      actionTime: action.time,
+
+      contactId: action.contactId,
+      benefId: action.benefId
+    };
+
+  case RECEIVED_GET_GEST_CONTACTS_FICHE_CONTACT:
+    return {
+      ...state,
+      isFetchingAll: false,
+      isFetchingIdentite: action.isFetching,
+      isFetchingContact: action.isFetching,
+      isFetchingDossiers: action.isFetching,
+      lastFetchTimeIdentite: action.time,
+      lastFetchTimeContact: action.time,
+      lastFetchTimeDossiers: action.time,
+      actionTime: action.time,
+
+      // contactId may be null if creation
+      contactId: action.gestBenef && action.gestBenef.contactId
+        ? action.gestBenef.contactId
+        : initialState.contactId,
+      // benefId should always be returned (as id) since no contact possible without a benef
+      benefId: action.gestBenef && action.gestBenef.id
+        ? action.gestBenef.id
+        : initialState.id,
+
+      // identité
+      civilite: action.gestBenef && action.gestBenef.civilite
+        ? action.gestBenef.civilite
+        : initialState.civilite,
+      nom: action.gestBenef && action.gestBenef.nom
+        ? action.gestBenef.nom
+        : initialState.nom,
+      nomJeuneFille: action.gestBenef && action.gestBenef.nomJeuneFille
+        ? action.gestBenef.nomJeuneFille
+        : initialState.nomJeuneFille,
+      prenom: action.gestBenef && action.gestBenef.prenom
+        ? action.gestBenef.prenom
+        : initialState.prenom,
+      dateNaissance: action.gestBenef && action.gestBenef.dateNaissance
+        ? action.gestBenef.dateNaissance
+        : initialState.dateNaissance,
+      numss: action.gestBenef && action.gestBenef.numss
+        ? action.gestBenef.numss
+        : initialState.numss,
+      dateDeces: action.gestBenef && action.gestBenef.dateDeces
+        ? action.gestBenef.dateDeces
+        : initialState.dateDeces,
+      maritalStatus: action.gestBenef && action.gestBenef.maritalStatus
+        ? action.gestBenef.maritalStatus
+        : initialState.maritalStatus,
+      // contact data
+      fixedPhone: action.gestBenef && action.gestBenef.fixedPhone
+        ? action.gestBenef.fixedPhone
+        : initialState.fixedPhone,
+      mobilePhone: action.gestBenef && action.gestBenef.mobilePhone
+        ? action.gestBenef.mobilePhone
+        : initialState.mobilePhone,
+      email: action.gestBenef && action.gestBenef.email
+        ? action.gestBenef.email
+        : initialState.email,
+      // contact data > sub: adress
+      numAdress: action.gestBenef && action.gestBenef.numAdress
+        ? action.gestBenef.numAdress
+        : initialState.numAdress,
+      voie: action.gestBenef && action.gestBenef.voie
+        ? action.gestBenef.voie
+        : initialState.voie,
+      complementAdr: action.gestBenef && action.gestBenef.complementAdr
+        ? action.gestBenef.complementAdr
+        : initialState.complementAdr,
+      codePostal: action.gestBenef && action.gestBenef.codePostal
+        ? action.gestBenef.codePostal
+        : initialState.codePostal,
+      ville: action.gestBenef && action.gestBenef.ville
+        ? action.gestBenef.ville
+        : initialState.ville,
+      pays: action.gestBenef && action.gestBenef.pays
+        ? action.gestBenef.pays
+        : initialState.pays,
+      // dossiers:
+      dossiers: action.gestBenef && action.gestBenef.dossiers
+        ? [...action.gestBenef.dossiers]
+        : [...initialState.dossiers]
+    };
+
+  case ERROR_GET_GEST_CONTACTS_FICHE_CONTACT:
+    return {
+      ...state,
+      isFetchingContact: action.isFetchingContact,
+      lastFetchTimeContact: action.time,
+      actionTime: action.time,
+      error: action.error
+    };
+
+
   // /////////////////
   // fiche activite
   // /////////////////
