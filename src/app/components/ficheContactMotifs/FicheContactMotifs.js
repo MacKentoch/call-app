cd496a994174224bad369037767f1a1d5da69689
MacKentoch <erwan.datin@gmail.com>
2016-10-14 15:37:17 +0200

    FicheContactMotifs

--- src/app/components/ficheContactMotifs/FicheContactMotifs.js
+++ src/app/components/ficheContactMotifs/FicheContactMotifs.js
@@ -22,34 +22,119 @@ class FicheContactMotifs extends Component {
 
   render() {
     const {
-      isEditing,
+      saveMotifs,
+      addnewMotifs,
 
-      idNiveau2,
-      labelNiveau2,
-      valueNiveau2,
-      listMotifsNiveau2,
-      onChangeNiveau2,
-
-      idNiveau3,
-      labelNiveau3,
-      valueNiveau3,
-      listMotifsNiveau3,
-      onChangeNiveau3,
-
-      idNiveau4,
-      labelNiveau4,
-      valueNiveau4,
-      listMotifsNiveau4,
-      onChangeNiveau4
+      listMotifs
     } = this.props;
 
     return (
       <div className="row">
         <div className="col-xs-3">
-          
+
+          <div className="row">
+            <div className="col-xs-12">
+              <label
+                className="control-label"
+                htmlFor="inputDateCreation">
+                Motifs
+              </label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-12">
+              {
+                isEditing &&
+                <a
+                  className="btn btn-primary btn-block mailBoxNewEmailButton_button"
+                  style={{width: '120px'}}
+                  onClick={saveMotifs}>
+                  <i className="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;
+                  Enregister
+                </a>
+              }
+              {
+                !isEditing &&
+                <a
+                  className="btn btn-primary btn-block mailBoxNewEmailButton_button"
+                  style={{width: '120px'}}
+                  onClick={addnewMotifs}>
+                  <i className="fa fa-plus" aria-hidden="true"></i>&nbsp;
+                  Ajouter
+                </a>
+              }
+            </div>
+          </div>
+
         </div>
         <div className="col-xs-9">
+          {
+            listMotifs.map(
+              (motif, MotifIdx) => {
+                const {
+                  isEditing,
+                  idNiveau2,
+                  labelNiveau2,
+                  valueNiveau2,
+                  listMotifsNiveau2,
+                  onChangeNiveau2,
+                  idNiveau3,
+                  labelNiveau3,
+                  valueNiveau3,
+                  listMotifsNiveau3,
+                  onChangeNiveau3,
+                  idNiveau4,
+                  labelNiveau4,
+                  valueNiveau4,
+                  listMotifsNiveau4,
+                  onChangeNiveau4
+                } = motif;
 
+                if (isEditing) {
+                  return (
+                    <div className="row">
+                      <div className="col-xs-4">
+                        <MotifDropDown
+
+                        />
+                      </div>
+                      <div className="col-xs-4">
+                        <MotifDropDown
+
+                        />
+                      </div>
+                      <div className="col-xs-4">
+                        <MotifDropDown
+
+                        />
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div className="row">
+                      <div className="col-xs-4">
+                        <LabelMotif
+
+                        />
+                      </div>
+                      <div className="col-xs-4">
+                        <LabelMotif
+
+                        />                        
+                      </div>
+                      <div className="col-xs-4">
+                        <LabelMotif
+
+                        />
+                      </div>
+                    </div>
+                  );
+                }
+              }
+            )
+          }
         </div>
       </div>
     );
@@ -57,25 +142,32 @@ class FicheContactMotifs extends Component {
 }
 
 FicheContactMotifs.propTypes = {
-  isEditing: PropTypes.bool,
+  saveMotifs: PropTypes.func.isRequired,
+  addnewMotifs: PropTypes.func.isRequired,
 
-  idNiveau2: PropTypes.string.isRequired,
-  labelNiveau2: PropTypes.string.isRequired,
-  valueNiveau2: PropTypes.string,
-  listMotifsNiveau2: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onChangeNiveau2: PropTypes.func.isRequired,
+  listMotifs: PropTypes.arrayOf(
+    PropTypes.shape({
+      isEditing: PropTypes.bool,
 
-  idNiveau3: PropTypes.string.isRequired,
-  labelNiveau3: PropTypes.string.isRequired,
-  valueNiveau3: PropTypes.string,
-  listMotifsNiveau3: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onChangeNiveau3: PropTypes.func.isRequired,
+      idNiveau2: PropTypes.string.isRequired,
+      labelNiveau2: PropTypes.string.isRequired,
+      valueNiveau2: PropTypes.string,
+      listMotifsNiveau2: PropTypes.arrayOf(PropTypes.string).isRequired,
+      onChangeNiveau2: PropTypes.func.isRequired,
 
-  idNiveau4: PropTypes.string.isRequired,
-  labelNiveau4: PropTypes.string.isRequired,
-  valueNiveau4: PropTypes.string,
-  listMotifsNiveau4: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onChangeNiveau4: PropTypes.func.isRequired
+      idNiveau3: PropTypes.string.isRequired,
+      labelNiveau3: PropTypes.string.isRequired,
+      valueNiveau3: PropTypes.string,
+      listMotifsNiveau3: PropTypes.arrayOf(PropTypes.string).isRequired,
+      onChangeNiveau3: PropTypes.func.isRequired,
+
+      idNiveau4: PropTypes.string.isRequired,
+      labelNiveau4: PropTypes.string.isRequired,
+      valueNiveau4: PropTypes.string,
+      listMotifsNiveau4: PropTypes.arrayOf(PropTypes.string).isRequired,
+      onChangeNiveau4: PropTypes.func.isRequired
+    })
+  ).isRequired
 };
 
 FicheContactMotifs.defaultProps = {
