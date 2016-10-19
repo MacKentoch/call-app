46ae109810609a8711378296125623f7a5619da2
MacKentoch <erwan.datin@gmail.com>
2016-10-19 15:57:07 +0200

    ActiviteContent

--- src/app/views/gestContacts/ficheActivite/activiteContent/ActiviteContent.js
+++ src/app/views/gestContacts/ficheActivite/activiteContent/ActiviteContent.js
@@ -22,65 +22,9 @@ moment.locale('fr');
 const formatDate = appConfig.formatDate.defaut;
 
 const ActiviteContent = ({
-  benefId,
-  isCollapsedFicheContact,
-  onCollapseClick,
-  lastFetchTimeFicheContact,
-  isFetchingFicheContact,
-  isSavingFicheContact,
   dateCreationFicheContact,
   onDateCreationFicheContactChanged,
-  creeParFicheContact,
-  dateReceptionFicheContact,
-  onDateReceptionFicheContactChanged,
-  statutIndexFicheContact,
-  onStatutIndexFicheContactChanged,
-  listStatutFicheContact,
-  dateClotureFicheContact,
-  // onDateClotureFicheContactChanged,
-  clotureParFicheContact,
-  // onClotureParFicheContactChanged,
-  typeIndexFicheContact,
-  onTypeIndexFicheContactChanged,
-  listTypeFicheContact,
-  canalIndexFicheContact,
-  // onCanalIndexFicheContactChanged,
-  listCanauxFicheContact,
-  // onListCanauxFicheContactChanged,
-  numDossierIndexSelected,
-  onNumDossierIndexSelectedChanged,
-  listNumDossierFicheContact,
-  // onListNumDossierFicheContactChanged,
-  domaineFicheContact,
-  onDomaineFicheContactChanged,
-  statutBenefFicheContact,
-  // onStatutBenefFicheContactChanged,
-  listStatutBenefFicheContact,
-  attachmentsFicheContact,
-  // onAttachmentsFicheContactChanged,
-  commentaireFicheContact,
-  onCommentaireFicheContactChanged,
-  groupeDestinataireIsActive,
-  // onGroupeDestinataireIsActiveChanged,
-  groupeDestinataireIdSelected,
-  onGroupeDestinataireIdSelectedChanged,
-  listGroupeDestinataire,
-  // onListGroupeDestinataireChanged,
-  activites,
-  isSavingFicheNewActivite,
-  saveFicheContact,
 
-  listMotifsNiveau2,
-  listMotifsNiveau3,
-  listMotifsNiveau4,
-
-  addNewMotifs,
-  saveMotifs,
-  onRemoveMotifs,
-
-  onChangeNiveau2,
-  onChangeNiveau3,
-  onChangeNiveau4
 }) => {
   return (
     <form role="form">
@@ -90,215 +34,24 @@ const ActiviteContent = ({
         {/* date de création */}
         <div className="col-md-4">
           <DateInput
-            id="inputDateCreation"
+            id="inputDateCreationFicheActivite"
             label={'Date de création'}
             value={isValidDateOrReturnDefault(dateCreationFicheContact, formatDate)}
             onChange={onDateCreationFicheContactChanged}
           />
         </div>
+
         {/* créé par */}
         <div className="col-md-4">
-          <FormLabel
-            id="inputCreePar"
-            label={'créé par'}
-            value={creeParFicheContact}
-          />
+
         </div>
-        {/* date reception */}
+
+        {/* date de réception */}
         <div className="col-md-4">
-          <DateInput
-            id="inputDateReception"
-            label={'Date de réception'}
-            value={isValidDateOrReturnDefault(dateReceptionFicheContact, formatDate)}
-            onChange={onDateReceptionFicheContactChanged}
-          />
+
         </div>
       </div>
 
-      {/* 2nd row */}
-      <div className="row">
-        {/* statut */}
-        <div className="col-md-4">
-          <StatutFicheDropDown
-            id="statutFiche"
-            label={'Statut'}
-            value={statutIndexFicheContact}
-            onChange={onStatutIndexFicheContactChanged}
-            listeStatutFiche={listStatutFicheContact}
-          />
-        </div>
-        {/* date cloture */}
-        <div className="col-md-4">
-          {/* <DateInput
-            id="inputDateCloture"
-            label={'Date de clôture'}
-            value={isValidDateOrReturnDefault(dateClotureFicheContact, formatDate)}
-            onChange={onDateClotureFicheContactChanged}
-          /> */}
-          <FormLabel
-            id="inputDateCloture"
-            label={'Date de clôture'}
-            value={isValidDateOrReturnDefault(dateClotureFicheContact, formatDate)}
-          />
-        </div>
-        {/* cloture par */}
-        <div className="col-md-4">
-          {/* <TextInput
-            id="inputCloturePar"
-            label={'Clôturé par'}
-            value={clotureParFicheContact}
-            onChange={onClotureParFicheContactChanged}
-          /> */}
-          <FormLabel
-            id="inputCloturePar"
-            label={'Clôturé par'}
-            value={clotureParFicheContact}
-          />
-        </div>
-      </div>
-
-      <div style={{height: '20px'}}></div>
-
-      {/* 3rd row */}
-      <div className="row">
-        {/* type */}
-        <div className="col-md-12">
-          <TypeFicheContactNavPills
-            selectedValue={typeIndexFicheContact}
-            onChange={onTypeIndexFicheContactChanged}
-            listTypeFicheContact={listTypeFicheContact}
-          />
-        </div>
-      </div>
-
-      <div style={{height: '15px'}}></div>
-
-      {/* 4th row */}
-      <div className="row">
-        {/* canal */}
-        <div className="col-xs-3">
-          <FormLabel
-            id="labelCanal"
-            label={'Canal'}
-            value={listCanauxFicheContact[canalIndexFicheContact] ? listCanauxFicheContact[canalIndexFicheContact] : ' --- '}
-          />
-        </div>
-
-        {/* nums dossier */}
-        <div className="col-xs-3">
-          <NumsDossiersDropDown
-            id="numsDossiers"
-            label={'N° Dossier'}
-            benefId={benefId}
-            value={numDossierIndexSelected}
-            onChange={onNumDossierIndexSelectedChanged}
-            listNumDossierFicheContact={listNumDossierFicheContact}
-          />
-        </div>
-
-        {/* domaine */}
-        <div className="col-xs-3">
-          <FormLabel
-            id="labelDomaineFicheContact"
-            label={'Domaine'}
-            value={domaineFicheContact}
-          />
-        </div>
-
-        {/* statut */}
-        <div className="col-xs-3">
-          {/* <StatutBenefFicheContactDropDown
-            id="statutBenefDropDownFicheContact"
-            label={'Statut'}
-            value={statutBenefFicheContact}
-            onChange={onStatutBenefFicheContactChanged}
-            listStatutBenefFicheContact={listStatutBenefFicheContact}
-          /> */}
-          <FormLabel
-            id="labelStatut"
-            label={'Statut'}
-            value={listStatutBenefFicheContact[statutIndexFicheContact] ? listStatutBenefFicheContact[statutIndexFicheContact] : ' --- '}
-          />
-        </div>
-      </div>
-
-      <div style={{height: '20px'}}></div>
-
-      <div className="row">
-        <div className="col-xs-12">
-          <FicheContactMotifs
-            saveMotifs={saveMotifs}
-            isSavingFicheNewActivite={isSavingFicheNewActivite}
-            addNewMotifs={addNewMotifs}
-            onRemoveMotifs={onRemoveMotifs}
-            listMotifs={activites}
-            labelNiveau2={'Niveau 2'}
-            labelNiveau3={'Niveau 3'}
-            labelNiveau4={'Niveau 4'}
-            listMotifsNiveau2={listMotifsNiveau2}
-            listMotifsNiveau3={listMotifsNiveau3}
-            listMotifsNiveau4={listMotifsNiveau4}
-            onChangeNiveau2={onChangeNiveau2}
-            onChangeNiveau3={onChangeNiveau3}
-            onChangeNiveau4={onChangeNiveau4}
-          />
-        </div>
-      </div>
-
-      <div style={{height: '20px'}}></div>
-
-      {/* 5th row */}
-      <div className="row">
-        <div
-          className="col-xs-12">
-          <ListAttachments
-            id={'ficheContactPJ'}
-            label={attachmentsFicheContact.length > 0 ?  `Pièces jointes reçus (${attachmentsFicheContact.length})` : 'Pièces jointes reçus'}
-            attachments={attachmentsFicheContact}
-            onDelete={()=>console.log('on fiche contact delete attachments: disabled')}
-          />
-        </div>
-      </div>
-      {/* 6th row */}
-      <div className="row">
-        <div className="col-xs-12">
-          <TextAreaInput
-            id={'ficheContactCommentaireTextArea'}
-            label={'Commentaires'}
-            value={commentaireFicheContact}
-            nbrows={3}
-            onChange={onCommentaireFicheContactChanged}
-          />
-        </div>
-
-      </div>
-      {/* <div style={{height: '20px'}}></div> */}
-
-      {/* 7th row */}
-      <div className="row">
-        <div className="col-xs-6 ">
-          <div className="pull-right">
-            <FicheContactGroupeDestinataireDropDown
-              id={'groupdDestDropDownFicheContact'}
-              label={''}
-              value={groupeDestinataireIdSelected}
-              onChange={onGroupeDestinataireIdSelectedChanged}
-              groupDestinataires={listGroupeDestinataire}
-            />
-          </div>
-        </div>
-        <div className="col-xs-6">
-          {/* buttons */}
-          <a
-            className="btn btn-primary btn-block mailBoxNewEmailButton_button"
-            style={{width: '120px'}}
-            onClick={saveFicheContact}>
-            <i className="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;
-            Enregister
-          </a>
-        </div>
-      </div>
-      <div style={{height: '20px'}}></div>
     </form>
   );
 };
