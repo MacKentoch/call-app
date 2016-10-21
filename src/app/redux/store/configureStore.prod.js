dcd2a9f129268e363d9cd801cb7a4ac8da62773a
MacKentoch <erwan.datin@gmail.com>
2016-10-21 10:15:10 +0200

    prod store

--- src/app/redux/store/configureStore.prod.js
+++ src/app/redux/store/configureStore.prod.js
@@ -6,19 +6,19 @@ import {
   compose,
   combineReducers
 }                         from 'redux';
-import createLogger       from 'redux-logger';
+// import createLogger       from 'redux-logger';
 import thunkMiddleware    from 'redux-thunk';
 import * as reducers      from '../reducers';
 import { routerReducer }  from 'react-router-redux';
 
-const loggerMiddleware = createLogger({
-  level     : 'info',
-  collapsed : true
-});
+// const loggerMiddleware = createLogger({
+//   level     : 'info',
+//   collapsed : true
+// });
 
 // createStore : enhancer
 const enhancer = compose(
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
+  applyMiddleware(thunkMiddleware)
 );
 
 // combine reducers
